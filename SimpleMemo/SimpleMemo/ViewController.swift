de5899689b70db2a1e33d139fcda68ba67da438d
sherry <yeahaluu@gmail.com>
2022-09-07 22:48:12 +0900

    Feat: Create SimpleMemo project

---
+++ SimpleMemo/SimpleMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMemo
+//
+//  Created by YejiMac on 2022/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
