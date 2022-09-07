a4c723fa0d18dfe94eac71d884dd958a5aa7bd47
Taekhwan Han <hth3396@gmail.com>
2022-09-07 23:02:46 +0900

    init Project

---
+++ SimpleMemo_Taek/SimpleMemo_Taek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMemo_Taek
+//
+//  Created by 한택환 on 2022/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
