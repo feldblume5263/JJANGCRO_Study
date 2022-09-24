29b88e485c2b328d9a18185d9b2459a5f27abd00
Nick(Seungyun Kim) <sy5072@naver.com>
2022-09-25 01:07:27 +0900

    [CHORE] MemoView 하단에 버튼 레이아웃 구현 (#2)

--- SimpleMemoApp_Nick/SimpleMemoApp_Nick/Controller/MemoViewController.swift
+++ SimpleMemoApp_Nick/SimpleMemoApp_Nick/Controller/MemoViewController.swift
@@ -16,6 +16,42 @@ class MemoViewController: UIViewController {
         return textView
     }()
     
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let makeCheckListButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "checklist"), for: .normal)
+        button.tintColor = .systemYellow
+        return button
+    }()
+    
+    private let addPhotoButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "camera"), for: UIControl.State.normal)
+        button.tintColor = .systemYellow
+        return button
+    }()
+    
+    private let showPencilKitButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "pencil.tip.crop.circle"), for: UIControl.State.normal)
+        button.tintColor = .systemYellow
+        return button
+    }()
+    
+    private let writeMemoButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "square.and.pencil"), for: UIControl.State.normal)
+        button.tintColor = .systemYellow
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         render()
@@ -23,12 +59,24 @@ class MemoViewController: UIViewController {
     }
     
     private func render() {
+        
+        view.addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(makeCheckListButton)
+        buttonStackView.addArrangedSubview(addPhotoButton)
+        buttonStackView.addArrangedSubview(showPencilKitButton)
+        buttonStackView.addArrangedSubview(writeMemoButton)
+        buttonStackView.constraint(leading: view.leadingAnchor,
+                                   bottom: view.bottomAnchor,
+                                   trailing: view.trailingAnchor)
+        buttonStackView.constraint(buttonStackView.heightAnchor, constant: 100)
+        
         view.addSubview(memoTextView)
         memoTextView.constraint(top: view.safeAreaLayoutGuide.topAnchor,
                                 leading: view.safeAreaLayoutGuide.leadingAnchor,
-                                bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                bottom: makeCheckListButton.topAnchor,
                                 trailing: view.safeAreaLayoutGuide.trailingAnchor,
-                                padding: UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24))
+                                padding: UIEdgeInsets(top: 0, left: 24, bottom: 50, right: 24))
+        
     }
     
     private func configUI() {
